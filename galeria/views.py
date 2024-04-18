b15e9df2e04d5c26fbb5f4f8015935c6d8e6da9c
pedromrtns <90918013+pedromrtns@users.noreply.github.com>
2024-04-18 17:31:55 -0300

    referencias

--- galeria/views.py
+++ galeria/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
+from galeria.models import Fotografia
 
 def index(request):
-    
-    dados = {
-        1: {"nome": "Nebulosa de Carina",
-            "legenda": "webbtelecope.org/NASA/James Webb"},
-        2: {"nome": "Galaxia NGC 1079",
-            "legenda": "nasa.org/NASA/Huble"}
-    }
-    
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
